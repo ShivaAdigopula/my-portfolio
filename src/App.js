bbf39a166696f9598e8e53342db233bef5caf738
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-07-09 23:55:40 +0400

    moved menu to floating action button

--- src/App.js
+++ src/App.js
@@ -7,23 +7,35 @@ import { data } from './assets/data';
 import { ExperienceList } from './components/experienceList';
 import { Fade, Zoom } from 'react-reveal';
 import { SkillsComponent } from './components/skills';
-import { Grid, Link } from '@mui/material';
+import { Grid, Link, SpeedDial, SpeedDialAction } from '@mui/material';
+import { GiHamburgerMenu, GiSkills } from 'react-icons/gi';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { MdWork , MdClose} from 'react-icons/md';
 
 function App() {
   const { name, designation, about, experience, socialNetworks, skills, resume } = data;
+
+  const actions = [
+    { icon: <Link  href="#about">< BsFillPersonFill size={25} /></Link>, name: 'About' },
+    { icon: <Link  href="#experience"><MdWork size={25} /></Link>, name: 'Experience' },
+    { icon: <Link  href="#skills"><GiSkills size={25}/></Link>, name: 'Skills' },
+  ];
   return (<Fragment>
-    <Grid container className='nav-bar'
-      flexDirection="column"
-      justifyContent="flex-end"
-      alignItems="flex-end"
-     
-      columnSpacing={4}>
-      
-      <Grid item className='nav-bar-item'><Link style={{textDecoration: 'none', color: 'inherit'}} href="#about">About</Link></Grid>
-        <Grid item className='nav-bar-item'><Link style={{textDecoration: 'none', color: 'inherit'}} href="#experience">Experience</Link></Grid>
-        <Grid item className='nav-bar-item'><Link style={{textDecoration: 'none', color: 'inherit'}} href="#skills">Skills</Link></Grid>
-      
-    </Grid>
+    <SpeedDial
+      FabProps={{ 'color': 'secondary'}}
+      ariaLabel="Menu"
+      sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      icon={<GiHamburgerMenu size="25" />}
+      openIcon={<MdClose size={25} />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
     <Fade >
       <IntroComponent name={name} designation={designation} socialNetworks={socialNetworks} />
     </Fade>
